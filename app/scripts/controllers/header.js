4d6575858fddcb76139dab3f970d3ef993a4fe0d
Akanksha <akanksha@Akankshas-MacBook-Pro.local>
2016-09-14 18:00:18 -0700

    home finish

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -8,17 +8,42 @@
  * Controller of the getReadyNewApp
  */
 angular.module('getReadyNewApp')
-    .controller('HeaderCtrl', function($scope, $location, userService,  $rootScope) {
+    .controller('HeaderCtrl', function($scope, $location, userService, $rootScope, $anchorScroll) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
-        
+
         $scope.isLoggedIn = true;
         $scope.isLoggedOut = true;
         $scope.isRegistered = true;
-        
+
+        var loginSessionData = userService.getSessionToken();
+        if (loginSessionData !== undefined) {
+            $rootScope.accType = loginSessionData.currentUser.accType;
+        }
+
+        $scope.toggled = function(open) {
+            // $log.log('Dropdown is now: ', open);
+        };
+        $scope.getSeekerlink = function(accType) {
+            console.log(accType);
+
+            if (loginSessionData !== undefined) {
+
+                if ($rootScope.accType === accType) {
+                    $location.path('/' + accType);
+                } else {
+                    $location.path('/login');
+                }
+
+            } else {
+                $rootScope.accType = accType;
+                $location.path('/login');
+            }
+
+        }
         $scope.logout = function() {
             // console.log('logged out');
             userService.removeSessionToken();
@@ -30,15 +55,21 @@ angular.module('getReadyNewApp')
 
 
         $scope.init = function() {
-            
+
             var loginSessionData = userService.getSessionToken();
             // console.log(loginSessionData);
             if (loginSessionData !== undefined) {
                 // console.log('header');
                 $scope.isLoggedIn = false;
                 $scope.isLoggedOut = false;
-            } 
+            }
         };
 
         $scope.init();
+
+        $scope.scrollTo = function(id) {
+            $location.hash(id);
+            console.log($location.hash());
+            $anchorScroll();
+        };
     });
